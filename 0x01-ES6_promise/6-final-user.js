a902db555010690460a7725a0bececbbde1f047c
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-11-30 20:14:09 +0000

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,27 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+
+    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+
+    return [
+      {
+        status: userResult.status,
+        value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason,
+      },
+      {
+        status: photoResult.status,
+        value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason,
+      },
+    ];
+  } catch (error) {
+    console.error(error);
+    return []; // Return an empty array if an unexpected error occurs
+  }
+}
+
+export default handleProfileSignup;
