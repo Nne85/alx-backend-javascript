37e6d2a95cdd29695ed6a54d3b1984a7d1b749c7
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-12-03 14:20:23 +0000

    Currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -4,7 +4,23 @@ export default class Currency {
     this._name = name;
   }
 
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
   displayFullCurrency() {
-    return ` ${this._name}  (${this._code})`;
+    return `${this._name} (${this._code})`;
   }
 }
