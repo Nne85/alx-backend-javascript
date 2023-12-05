b5dff6f74467698821a52147b594ca3e52b8b8c5
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-12-05 23:42:03 +0000

    New commits

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,14 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+
+  const int8Array = new Int8Array(buffer);
+
+  // Set the value at the specified position
+  int8Array[position] = value;
+
+  return new DataView(buffer);
+}
