42e5c48f5f1c5efdd272adc4930830ebb45513c9
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-12-06 00:37:58 +0000

    Grocery

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -2,6 +2,7 @@ export default function groceriesList() {
   const grocery = new Map();
   grocery.set('Apples', 10);
   grocery.set('Tomatoes', 10);
+  grocery.set('Rice', 1);
   grocery.set('Pasta', 1);
   grocery.set('Banana', 5);
 
