c9a4ba978de4a447c0c3fbdfba45f40c9e055864
Nnenna Kehinde <nneukamaka@yahoo.com>
2024-02-03 21:22:09 +0000

    Test with Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,34 @@
+// 5-payment.test.js
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    // Create a spy on console.log before each test
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    // Restore the spy after each test
+    consoleLogSpy.restore();
+  });
+
+  it('should log correct message for totalAmount=100 and totalShipping=20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    // Verify that console.log is called with the correct message and only once
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWithExactly('The total is: 120')).to.be.true;
+  });
+
+  it('should log correct message for totalAmount=10 and totalShipping=10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    // Verify that console.log is called with the correct message and only once
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWithExactly('The total is: 20')).to.be.true;
+  });
+});
